ab7009bb0f5d145165a7d8378f54f423379151fa
nyyyannn <nayanshekartg@gmail.com>
2025-12-01 07:06:30 +0530

    fibonacci sequence using omp to demonstrate parallelism

---
+++ program3.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<omp.h>
+
+long long fib(int n)
+{
+    if(n<=1)
+    {
+        return n;
+    }
+    long long x,y;
+
+    #pragma omp task shared(x)
+    x=fib(n-1);
+
+    #pragma omp task shared(y)
+    y=fib(n-2);
+
+    #pragma omp taskwait
+    return x+y;
+}
+int main()
+{
+    int n;
+    printf("Enter n:");
+    scanf("%d",&n);
+    
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            for(int i=0;i<n;i++)
+            {
+                long long res=fib(i);
+                printf("Fib(%d): %lld\n",i,res);
+            }
+        }
+    }
+    return 0;
+}
