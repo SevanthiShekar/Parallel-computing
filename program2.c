ae003f5d0d582c5436ad5e1a79ce0ebb0ce3a9b2
Sevanthi Shekar <157514974+SevanthiShekar@users.noreply.github.com>
2026-01-15 19:49:24 +0530

    Implement OpenMP program for chunked iterations
    Added OpenMP program to divide iterations into chunks of 2.

--- program2.c
+++ program2.c
@@ -1,3 +1,12 @@
+/*Question 2:
+Write an OpenMP program that divides the Iterations into chunks containing 2 iterations,
+respectively (OMP_SCHEDULE=static,2). Its input should be the number of iterations, and
+its output should be which iterations of a parallelized for loop are executed by which thread.
+ For example, if there are two threads and four iterations, the output might be the following:
+a. Thread 0 : Iterations 0 −− 1
+b. Thread 1 : Iterations 2 −− 3
+*/
+
 #include<stdlib.h>
 #include <stdio.h>
 #include <omp.h>
